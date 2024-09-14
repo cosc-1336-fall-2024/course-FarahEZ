1a5d55ad7299999a5bbe0f3a5acce0af7046e537
FarahEZ <farah.el-zein@g.austincc.edu>
2024-09-14 21:42:36 +0000

    HW 2 (test Unit test)

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,11 @@
-#
+import decisions
+
+def main():
+    num=int(input("Enter Number 1: "))
+    num2=int(input("Enter Number 2: "))
+
+    ratio=decisions.get_options_ratio(num,num2)
+
+    print(decisions.get_faculty_rating(ratio))
+
+main()
