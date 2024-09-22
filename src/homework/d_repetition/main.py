d1dfda490554bb35f5e4d1257cf1330fe02128f5
FarahEZ <farah.el-zein@g.austincc.edu>
2024-09-22 02:55:51 +0000

    HW3

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,22 @@
-#
+import repetition
+
+def main():
+    menu=int(input("Enter number: 1 for factorial, 2 for sum of odd numbers, and 3 for exit."))
+
+    if (menu==1):
+        fac1=int(input("Enter a number "))
+        while (0>=fac1 or fac1>=10):
+            fac1=int(input("Enter a number between 0-10 "))
+        print(repetition.get_factorial(fac1))
+        print("Do you want to exit?")
+    if (menu==2):
+        odd=int(input("Enter a number "))
+        while (0>=odd or odd>=10):
+            odd=int(input("Enter a number between 0-10 "))
+        print(repetition.sum_odd_numbers(odd))
+        print("Do you want to exit?")
+    if (menu==3):
+        print("Do you want to continue?")
+
+main()
+
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,17 @@
-#
+
+def get_factorial(num):
+
+    num2=1
+
+    for i in range (1,num+1):
+        num2=num2*i
+     #   print(i)
+    return num2
+
+def sum_odd_numbers(num):
+    i=1
+    sum=0
+    while (i<=num):
+        sum=sum+i
+        i+=2
+    return sum
