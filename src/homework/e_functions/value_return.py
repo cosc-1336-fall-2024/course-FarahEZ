303795e661adf06384691323e41d32c668b543a0
FarahEZ <farah.el-zein@g.austincc.edu>
2024-09-29 02:52:00 +0000

    HW4

--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,16 @@
-#
+#HW 4 (Use Python to create a clock program with a Windows interface)
+
+def get_hours(epoch_seconds):
+    result= epoch_seconds // 3600
+    remainder= result % 24
+    return remainder
+
+def get_minutes(epoch_seconds):
+    result= epoch_seconds // 60
+    remainder= result % 60
+    return remainder
+
+def get_seconds(epoch_seconds):
+    result= epoch_seconds % 3600
+    remainder= result % 60
+    return remainder
