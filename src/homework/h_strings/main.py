1b736bc4212c5952f2dabf51861f1ae38aefcbaf
FarahEZ <farah.el-zein@g.austincc.edu>
2024-10-06 03:14:07 +0000

    HW5 DNA strings

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,22 @@
-#
+#HW 5
+import strings
+
+def main():
+    menu=int(input("Enter number: 1 for Hamming Distance, 2 for Dna Complement, and 3 for exit."))
+    while menu !=3:
+        if (menu==1):
+             dna1=input("Enter DNA string composed of C,T,A, and G: ")
+             dna2=input("Enter a second DNA string composed of C,T,A, and G: ") 
+
+             print(strings.get_hamming_distance(dna1,dna2))
+        
+        if (menu==2):
+            dna=input("Enter DNA string composed of C,T,A, and G: ")
+
+            print(strings.get_dna_complement(dna))
+        
+        else:
+            break
+        menu=int(input("Enter number: 1 for Hamming Distance, 2 for Dna Complement, and 3 for exit."))
+
+main()
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,23 @@
-#
+#HW 5
+
+def get_hamming_distance(dna1,dna2):
+   count=0
+
+   for i in range(0,len(dna1)):
+      if dna1[i]!= dna2[i]:
+         count+=1
+   return count 
+
+def get_dna_complement(dna):
+
+   comp = ""
+   for i in range(0,len(dna)):
+      if dna[i]=="C":
+         comp="G"+comp
+      elif dna[i]=="G":
+         comp="C"+comp
+      elif dna[i]=="A":
+         comp="T"+comp
+      elif dna[i]=="T":
+         comp="A"+comp
+   return comp
